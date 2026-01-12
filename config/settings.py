a6db28143c0b1f6c0983fa6a84858fa84c028165
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-01-11 23:56:40 -0300

    Temporary auto-create superuser on Render

--- config/settings.py
+++ config/settings.py
@@ -71,11 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 # ──────────────── BASE DE DATOS ────────────────
+import dj_database_url
+import os
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
     )
 }
+
 
 # ──────────────── LOCALIZACIÓN ────────────────
 LANGUAGE_CODE = "en-us"
