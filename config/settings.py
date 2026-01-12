215e8370988e7786f3f6c1e8898a67ea13c1977d
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-01-12 14:02:02 -0300

    Temporary auto-create superuser on Render

--- config/settings.py
+++ config/settings.py
@@ -75,10 +75,13 @@ import dj_database_url
 import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 
 # ──────────────── LOCALIZACIÓN ────────────────
