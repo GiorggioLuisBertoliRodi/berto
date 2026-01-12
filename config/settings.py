d0bb9d037330cfda13b4466eea80e2b0d1094ef5
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-01-11 23:19:55 -0300

    Temporary auto-create superuser on Render

--- config/settings.py
+++ config/settings.py
@@ -97,14 +97,3 @@ cloudinary.config(
 # ──────────────── DEFAULT AUTO FIELD ────────────────
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-if os.environ.get("RENDER"):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="bertolirodigiorggio@gmail.com",
-            password="admin1234"
-        )
-
