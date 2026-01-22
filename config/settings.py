ece1b1f1173dfe5e5644a1b3c3eaedb6cc9ed6c1
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-01-22 15:43:02 -0300

    Listo para produccion

--- config/settings.py
+++ config/settings.py
@@ -57,11 +57,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
