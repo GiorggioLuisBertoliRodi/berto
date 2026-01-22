0220b25b9fcb5a2d87bcde7da0d5e6140fc066e0
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-01-22 20:19:57 -0300

    Listo para produccion

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,8 @@ load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 DEBUG = os.environ.get("DEBUG") == "True"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
