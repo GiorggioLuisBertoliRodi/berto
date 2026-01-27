1571c80ffd213bb1fd93fcc4947fe4596a9d7534
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-01-27 17:24:29 -0300

    catalogo

--- catalogo/views.py
+++ catalogo/views.py
@@ -7,7 +7,7 @@ def home(request):
     productos_list = list(Producto.objects.all())
     Productos_home = random.sample(productos_list, min(len(productos_list), 5))
     categorias = Categoria.objects.all()
-    return render(request, 'home.html', {"Productos_home": Productos_home,"categorias": categorias})
+    return render(request, 'catalogo/home.html', {"Productos_home": Productos_home,"categorias": categorias})
 def busqueda_resultado(request):
     busqueda = request.GET.get('busqueda', '')  
     if busqueda:
@@ -15,17 +15,17 @@ def busqueda_resultado(request):
         Coincidencias = Paginator(Coincidencias,5)
         page_number = request.GET.get('page')
         page_obj = Coincidencias.get_page(page_number)
-        return render(request, 'busqueda_resultado.html', {'productos': page_obj})
+        return render(request, 'catalogo/busqueda_resultado.html', {'productos': page_obj})
 def detalles(request,id):
     producto_detalles = get_object_or_404(Producto,id=id)
-    return render(request,'Detalles.html',{'producto_detalles':producto_detalles})
+    return render(request,'catalogo/Detalles.html',{'producto_detalles':producto_detalles})
 
 def productos_por_categoria(request, id):
     categoria = get_object_or_404(Categoria, id=id)
     productos = Producto.objects.filter(categoria=categoria)
     categorias = Categoria.objects.all()
 
-    return render(request, 'productos_por_categoria.html', {
+    return render(request, 'catalogo/productos_por_categoria.html', {
         'Productos_home': productos,
         'categorias': categorias,
         'categoria_actual': categoria
