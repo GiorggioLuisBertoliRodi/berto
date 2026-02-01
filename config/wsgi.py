7e4bd95467783817ab486bc326d76fd82c439795
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-02-01 17:10:42 -0300

    Error config

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,13 +1,7 @@
-import sys
 import os
-
-# Ruta de tu proyecto
-project_home = '/home/tu_usuario/berto'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-# Nombre de tu proyecto Django (la carpeta con settings.py)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'berto.settings'
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 application = get_wsgi_application()
+
