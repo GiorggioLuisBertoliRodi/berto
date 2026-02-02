1a75addba97b9ca3dbb712f3656e2473a796fc6b
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-02-01 23:39:59 -0300

    Fix Categoria.nombre column case in Postgres

--- config/settings.py
+++ config/settings.py
@@ -101,14 +101,9 @@ TEMPLATES = [
 # DATABASE (PostgreSQL - Render)
 # ===============================
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),  # Render lo maneja internamente
-        'PORT': os.environ.get('DB_PORT'),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # ===============================
