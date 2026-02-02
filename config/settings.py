b7f76eea4cbd804f6da1390942c4ad54e1fa1393
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-02-02 17:25:46 -0300

    Sincronizar DB con campo nombre en Producto

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True  # Render Free requiere SSL
+        ssl_require=True  # importante para Render
     )
 }
 
