866587848c81833f4bd0f5fc5b96317009d2eb82
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-02-01 21:28:24 -0300

    Preparado para deploy en Render

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('producto/<int:id>/', views.detalles, name='detalles'),
+    path('detalles/<int:id>/', views.detalles, name='detalles'),
     path('buscar/', views.busqueda_resultado, name='busqueda_resultado'),
     path('productos_por_categoria/<int:id>/', views.productos_por_categoria, name='productos_por_categoria'),
     path('ver_comentarios/<int:id>/',views.ver_comentarios,name='ver_comentarios'),
