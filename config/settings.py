124ab030e50421be654fe69b0a8c64d42c0ecd1c
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-02-05 12:35:41 -0300

    Error con debug False

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,8 @@ CSRF_TRUSTED_ORIGINS = ["https://berto-99uc.onrender.com"]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # ===============================
 # APPS
