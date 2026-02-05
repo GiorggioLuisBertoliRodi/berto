632de0125a1c40b697b2a2097f70bbc9a54487dc
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-02-05 13:09:12 -0300

    Modificacion del sitio del admin para los comentarios

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categoria, Producto
+from .models import Categoria, Producto,Comentario_Producto
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
@@ -9,3 +9,8 @@ class CategoriaAdmin(admin.ModelAdmin):
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ("nombre", "categoria", "precio", "imagen")
     search_fields = ("nombre",)
+admin.site.register(Comentario_Producto)
+class ComentarioProductoAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "producto", "comentario", "fecha")
+    search_fields = ("comentario", "usuario__username")
+    list_filter = ("producto", "fecha")
