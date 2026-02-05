84247930e1ba24dc9575cccd06595f87b560f1c1
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-02-05 16:27:13 -0300

    Modificacion menor

--- catalogo/apps.py
+++ catalogo/apps.py
@@ -6,17 +6,3 @@ from django.db.models.signals import post_migrate
 class CatalogoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalogo'
-
-    #def ready(self):
-        # Crear superusuario automáticamente en Render
-        #def create_admin(sender, **kwargs):
-        #    if getattr(settings, "RENDER", False):  # Solo en Render
-        #        User = get_user_model()
-        #        if not User.objects.filter(username="admin").exists():
-        #            User.objects.create_superuser(
-        #                username="admin",
-        #                email="",       # puedes poner un email si quieres
-        #                password="admin1234"
-        #            )
-
-        #post_migrate.connect(create_admin, sender=self)
