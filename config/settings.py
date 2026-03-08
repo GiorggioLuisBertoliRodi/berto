c28bf4b7ec8d879501dcdea15adff13aa1286bc6
GiorggioLuisBertoliRodi <bertolirodigiorggio@gmail.com>
2026-03-08 16:30:05 -0300

    fix csrf

--- config/settings.py
+++ config/settings.py
@@ -20,14 +20,14 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dev-key")
 
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
-).split(",")
+ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = os.environ.get(
+#    "ALLOWED_HOSTS",
+#    "localhost,127.0.0.1"
+#).split(",")
 
 # Agrega tu dominio de Render aquí
-CSRF_TRUSTED_ORIGINS = ["https://berto-99uc.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://berto-7.onrender.com"]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
@@ -102,7 +102,6 @@ DATABASES = {
         ssl_require=True  # importante para Render
     )
 }
-
 
 # ===============================
 # PASSWORD VALIDATORS
